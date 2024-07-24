e39b436af3cd74e49f3c4401e0dd9b7a0fedf093
itingtseng <42088639+itingtseng@users.noreply.github.com>
2024-07-24 14:58:37 -0400

    add key to the references

--- backend/db/migrations/20240723172429-create-spot.js
+++ backend/db/migrations/20240723172429-create-spot.js
@@ -17,7 +17,10 @@ module.exports = {
       ownerId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: { model: 'Users' },
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
         onDelete: 'CASCADE'
       },
       address: {
--- backend/db/migrations/20240723173624-create-review.js
+++ backend/db/migrations/20240723173624-create-review.js
@@ -17,13 +17,19 @@ module.exports = {
       userId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: { model: 'Users' },
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
         onDelete: 'CASCADE'
       },
       spotId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: { model: 'Spots' },
+        references: {
+          model: 'Spots',
+          key: 'id',
+        },
         onDelete: 'CASCADE'
       },
       review: {
