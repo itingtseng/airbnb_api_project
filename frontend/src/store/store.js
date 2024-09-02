d4c1d013bae2db058004b826b02ca2c80eb33c0d
itingtseng <42088639+itingtseng@users.noreply.github.com>
2024-09-02 17:59:29 -0400

    frontend-phase-0

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { thunk } from 'redux-thunk';
-import logger from 'redux-logger';
-
-
-
 
 const rootReducer = combineReducers({
 
@@ -13,23 +9,14 @@ let enhancer;
 if (import.meta.env.MODE === 'production') {
   enhancer = applyMiddleware(thunk);
 } else {
-//   const logger = (await import("redux-logger")).default;
+  const logger = (await import("redux-logger")).default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
 const configureStore = (preloadedState) => {
-    const store = createStore(rootReducer, preloadedState, enhancer);
-
-    // Expose the store for development
-    if (import.meta.env.MODE !== 'production') {
-        console.log('Setting window.store');
-    window.store = store;
-  }
-    
-  return store;
-
+    return createStore(rootReducer, preloadedState, enhancer);
 };
   
 
